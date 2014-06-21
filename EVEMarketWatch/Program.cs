a9cc3b451625eff71ac38e2bbdd81b83ed96f2f1
sinaptik <sin486@gmail.com>
2014-06-22 09:35:26 +1200

    Add simple svg map with websockets

--- EVEMarketWatch/Program.cs
+++ EVEMarketWatch/Program.cs
@@ -2,15 +2,13 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Web.Script.Serialization;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 using ZeroMQ;
 using EVEMarketWatch.Utils;
 using EVEMarketWatch.Core.Domain;
@@ -20,43 +18,58 @@ namespace EVEMarketWatch
 {
     class Program
     {
+        private static readonly List<MapService> Services = new List<MapService>();
+
         static void Main(string[] args)
         {
-            ConcurrentQueue<Order> incomingOrders = new ConcurrentQueue<Order>();
+            var incomingOrders = new ConcurrentQueue<Order>();
 
-            Thread receiveThread = new Thread(() => ReceiveOrders(incomingOrders));
+            var receiveThread = new Thread(() => ReceiveOrders(incomingOrders));
             receiveThread.Start();
 
             var db = new OrderStorage();
 
+            var wssv = new WebSocketServer("ws://localhost:8088");
+
+            wssv.AddWebSocketService<MapService>("/eve");
+            wssv.Start();
+            
             while (true)
             {
                 SaveToDatabase(incomingOrders, db);
             }
+
+            wssv.Stop();
         }
+
         private static void SaveToDatabase(ConcurrentQueue<Order> incomingOrders, OrderStorage db)
         {
-            List<Order> orderList = new List<Order>();
+            var orderList = new List<Order>();
 
             orderList.Clear();
             Order order;
             while (incomingOrders.TryDequeue(out order))
             {
                 orderList.Add(order);
+
+                foreach (var service in Services)
+                    service.SendIt(order.solarSystemID.ToString());
             }
 
-            db.AddOrders(orderList);
+            //db.AddOrders(orderList);
             //var orders = db.Orders;
 
             //Console.WriteLine(orders.Average(o => o.price) + "  - " + orders.Count());
-            if (orderList.Count > 0) Console.WriteLine(orderList.Count + " processed...");
+
+            if (orderList.Count > 0) 
+                Console.WriteLine(orderList.Count + " processed...");
         }
 
         private static void ReceiveOrders(ConcurrentQueue<Order> incomingOrders)
         {
             using (var context = ZmqContext.Create())
             {
-                using (var subscriber = context.CreateSocket(SocketType.SUB))
+                using (var subscriber = context.CreateSocket(ZeroMQ.SocketType.SUB))
                 {
                     //Connect to the first publicly available relay.
                     subscriber.Connect("tcp://relay-us-central-1.eve-emdr.com:8050");
@@ -69,8 +82,8 @@ namespace EVEMarketWatch
                         try
                         {
                             // Receive compressed raw market data.
-                            int size = 0;
-                            byte[] receivedData = new byte[65535];
+                            var size = 0;
+                            var receivedData = new byte[65535];
                             subscriber.Receive(receivedData, out size);
 
                             // The following code lines remove the need of 'zlib' usage;
@@ -79,21 +92,21 @@ namespace EVEMarketWatch
                             // the four last bytes which are the adler32 checksum and
                             // the two first bytes which are the 'zlib' header.
                             byte[] decompressed;
-                            byte[] choppedRawData = new byte[(size - 4)];
+                            var choppedRawData = new byte[(size - 4)];
                             Array.Copy(receivedData, choppedRawData, choppedRawData.Length);
                             choppedRawData = choppedRawData.Skip(2).ToArray();
 
                             // Decompress the raw market data.
-                            using (MemoryStream inStream = new MemoryStream(choppedRawData))
-                            using (MemoryStream outStream = new MemoryStream())
+                            using (var inStream = new MemoryStream(choppedRawData))
+                            using (var outStream = new MemoryStream())
                             {
-                                DeflateStream outZStream = new DeflateStream(inStream, CompressionMode.Decompress);
+                                var outZStream = new DeflateStream(inStream, CompressionMode.Decompress);
                                 outZStream.CopyTo(outStream);
                                 decompressed = outStream.ToArray();
                             }
 
                             // Transform data into JSON strings.
-                            string marketJson = Encoding.UTF8.GetString(decompressed);
+                            var marketJson = Encoding.UTF8.GetString(decompressed);
 
                             var obj = JsonConvert.DeserializeObject<DataInterchange>(marketJson);
 
@@ -110,6 +123,33 @@ namespace EVEMarketWatch
                         }
                     }
                 }
+            }
+        }
+
+        public class MapService : WebSocketService
+        {
+            public bool Connected { get; set; }
+
+            protected override void OnOpen()
+            {
+                Connected = true;
+                Services.Add(this);
+
+                base.OnOpen();
+            }
+
+            protected override void OnClose(CloseEventArgs e)
+            {
+                Connected = false;
+                Services.Remove(this);
+
+                base.OnClose(e);
+            }
+
+            public void SendIt(string message)
+            {
+                if (Connected)
+                    Send(message);
             }
         }
     }
