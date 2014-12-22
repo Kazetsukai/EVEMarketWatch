e51b80efd813339504d658848b93615f2ecf31e2
sinaptik <sin486@gmail.com>
2014-12-22 15:47:33 +1300

    Create storage folder

--- EVEMarketWatch.DealFinder/MainWindowViewModel.cs
+++ EVEMarketWatch.DealFinder/MainWindowViewModel.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using EVEMarketWatch.Core.Storage;
 
 namespace EVEMarketWatch.DealFinder
 {
